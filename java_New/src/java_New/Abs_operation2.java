556f76bf92ef32ac834bfc46ab8664a83e6af973
pushpender.singh_bit <pushpender.singh_bit@Pooja>
2023-04-11 10:13:45 +0530

    First commit

---
+++ java_New/src/java_New/Abs_operation2.java
@@ -0,0 +1,13 @@
+package java_New;
+
+public class Abs_operation2 extends abs_Operation {
+    
+	public void opr()
+	{
+		int x = 18;
+		int y = 77;
+		int z = x+y;
+		System.out.println("The addition of two number is: " + z);
+	}
+	
+}
